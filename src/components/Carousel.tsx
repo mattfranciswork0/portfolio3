682576170cbd8db9576fd52767d102b1582d6bee
Matthew Francis <mattfrancis888@gmail.com>
2022-12-27 10:43:05 -0500

    Chagned pictures and updated texts.

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -19,7 +19,7 @@ import habanerHome from "../img/habanero-home.jpg";
 import bobbyhill from "../img/bobbyhill.jpg";
 export const SLIDE_ABOUT_ME_DESC =
     "Software Development Engineer II at Lightspeed in Toronto, Canada. BSc Computer Science at Wilfrid Laurier University. Co-Founder of Starty.";
-export const STARTUP_DESC = `Starty is a platform for people to collaborate. Collaborating with accelerator and team to challenge decisions, address market research and pivot on ideas.`;
+export const STARTUP_DESC = `Starty is a social platform to help students innovate together & stand out to employers. Collaborating with accelerator and team to challenge decisions, address market research and pivot on ideas.`;
 export const WORK_PROJECTS_DESC =
     "Holiday recipes from employees in Habanero shared to over a dozen of clients such as Suncor & Petro-Canada.";
 export const SLIDE_PROJECTS_DESC =
